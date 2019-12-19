53415bac524536d7d39be3812ab1bc011f66af4f
Szymon Gruszczyński <gruszczynski01@gmail.com>
2019-12-19 19:44:15 +0100

    before login and register implementation

---
+++ app/src/main/java/com/peargrammers/flashcards/activities/Authentication/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.peargrammers.flashcards.activities.Authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.peargrammers.flashcards.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
