3cea4695134e5bf373c87e9366cdaa915de1698f
Szymon Gruszczyński <gruszczynski01@gmail.com>
2019-12-24 00:22:34 +0100

    save catalog and back - bug to do

---
+++ app/src/main/java/com/peargrammers/flashcards/viewmodels/management/AddCatalogViewModel.java
@@ -0,0 +1,4 @@
+package com.peargrammers.flashcards.viewmodels.management;
+
+public class AddCatalogViewModel {
+}
---
+++ app/src/main/java/com/peargrammers/technical/SingleLiveEvent.java
@@ -0,0 +1,4 @@
+package com.peargrammers.technical;
+
+public class SingleLiveEvent {
+}
